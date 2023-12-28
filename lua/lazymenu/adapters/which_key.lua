e08944f026eec9ce0a2be8eb36996016a7a15994
abeldekat <abel@nomail.com>
2023-12-28 20:38:06 +0100

    implemented remap which-key

---
+++ lua/lazymenu/adapters/which_key.lua
@@ -0,0 +1,17 @@
+---@class LazyMenuWhichKeyAdapter
+local M = {}
+
+---@param remap_cb fun(values_cb:fun(), to_change:table<string,string>)
+---@param to_change table<string,string>
+function M.setup(remap_cb, to_change)
+  local Plugin = require("lazy.core.plugin")
+  local values = Plugin._values
+  local values_decorated = remap_cb(values, to_change)
+
+  ---@diagnostic disable-next-line: duplicate-set-field
+  Plugin._values = function(root, plugin, prop, is_list)
+    return values_decorated(root, plugin, prop, is_list)
+  end
+end
+
+return M
--- lua/lazymenu/init.lua
+++ lua/lazymenu/init.lua
@@ -1,21 +1,41 @@
 --[[
-Rename leader c to leader C is special:
---> Mason: regular 
---> Format Injected Langs: regular
---> LSP:
-  The keys are created on_attach:
+Example: Rename leader c to leader C:
+--> Mason: from plugin
+--> Format Injected Langs: from plugin
+--> LSP items: The keys are created on_attach for each buffer:
   source action, code action, lsp info, rename
---> Keymaps.lua: Format and Line diagnostics
-
+--> Config.keymaps.lua: Format and Line diagnostics
+--> Which-key: the ["<leader>c"] = "+coding" definition in opts.defaults
 --]]
 local M = {}
 
+-- return the opts for this plugin
 ---@param adapter LazyMenuPluginAdapter
 ---@return LazyMenuOptions
 local function get_opts(adapter)
   local opts = adapter.get_opts()
   opts = require("lazymenu.config").setup(opts)
   return opts
+end
+
+---@param plugin LazyPlugin
+local function is_lazyvim_fragment(plugin)
+  return plugin and plugin._ and plugin._.module and plugin._.module:find("lazyvim", 1, true)
+end
+
+---@param to_change table<string,string>
+---@param leaders string[]
+---@return string[]
+local function reduce(to_change, leaders)
+  local result = {}
+  for key, value in pairs(to_change) do
+    for _, leader in ipairs(leaders) do
+      if key == leader then
+        result[key] = value
+      end
+    end
+  end
+  return result
 end
 
 -- Remaps keys defined in LazyVim's keymaps.lua
@@ -56,18 +76,54 @@ local function remap_lsp(resolve_lsp_cb, to_change)
   end
 end
 
+-- Remaps keys defined in the opts of LazyVim's which-key definitions
+-- Needs to hook into the merging of opts before calling config,
+-- because the keys are defined in opts and opts can be a function(see ui.lua, noice.nvim)
+---@param values_cb fun(root:LazyPlugin, plugin:LazyPlugin, prop:string, is_list?:boolean)
+---@param to_change table<string,string>
+local function remap_which_key(values_cb, to_change)
+  -- See lazy.core.plugins._values
+  ---@param root LazyPlugin
+  ---@param plugin LazyPlugin
+  ---@param prop string
+  ---@param is_list? boolean
+  return function(root, plugin, prop, is_list)
+    if not (plugin and plugin.name == "which-key.nvim" and prop == "opts" and is_lazyvim_fragment(plugin)) then
+      return values_cb(root, plugin, prop, is_list)
+    end
+
+    local result = values_cb(root, plugin, prop, is_list)
+    local new_keys = {}
+    for key, desc in pairs(result.defaults) do
+      local key_to_use = key
+      for key_to_change, new_key in pairs(to_change) do
+        if key:find(key_to_change, 1, true) then
+          key_to_use = key:gsub(key_to_change, new_key)
+          break
+        end
+      end
+      new_keys[key_to_use] = desc
+    end
+    result.defaults = new_keys
+    return result
+  end
+end
+
 -- Remaps keys in LazyVim's plugin definitions after the plugin has been added
 ---@param add_cb fun(_, plugin:LazyPlugin, results?:string[])
 ---@param to_change table<string,string>
-local function remap(add_cb, to_change)
+local function remap_plugins(add_cb, to_change)
   -- See lazy.core.plugins.spec.add
   ---@param plugin LazyPlugin
   ---@param results? string[]
   return function(_, plugin, results)
     local fragment_has_keys = plugin and type(plugin) == "table" and plugin.keys and type(plugin.keys) == "table"
     plugin = add_cb(_, plugin, results) -- add and merge the fragment
+    if not is_lazyvim_fragment(plugin) then
+      return plugin -- only change LazyVim's definitions
+    end
 
-    if fragment_has_keys and plugin._.module and plugin._.module:find("lazyvim", 1, true) then
+    if fragment_has_keys then
       plugin.keys = vim.tbl_map(function(lazy_mapping)
         for key, value in pairs(to_change) do
           if lazy_mapping[1]:find(key, 1, true) then
@@ -81,32 +137,23 @@ local function remap(add_cb, to_change)
   end
 end
 
----@param to_change table<string,string>
----@param leaders string[]
----@return string[]
-local function reduce(to_change, leaders)
-  local result = {}
-  for key, value in pairs(to_change) do
-    for _, leader in ipairs(leaders) do
-      if key == leader then
-        result[key] = value
-      end
-    end
-  end
-  return result
-end
+-- A dummy setup method. See on_hook.
+function M.setup(_) end
 
 -- The main init method, called when the import is required by lazy.nvim
 ---@param plugin_adapter LazyMenuPluginAdapter
+---@param which_key_adapter LazyMenuWhichKeyAdapter
 ---@param lsp_adapter LazyMenuLspAdapter
 ---@param keymaps_adapter LazyMenuKeymapsAdapter
-function M.on_hook(plugin_adapter, lsp_adapter, keymaps_adapter)
+---@return table
+function M.on_hook(plugin_adapter, which_key_adapter, lsp_adapter, keymaps_adapter)
   local opts = get_opts(plugin_adapter)
   if not opts or vim.tbl_isempty(opts) then
     return {}
   end
 
-  plugin_adapter.setup(remap, opts.to_change)
+  plugin_adapter.setup(remap_plugins, opts.to_change)
+  which_key_adapter.setup(remap_which_key, opts.to_change)
 
   local lsp_to_change = reduce(opts.to_change, lsp_adapter.leaders())
   if not vim.tbl_isempty(lsp_to_change) then
@@ -120,8 +167,5 @@ function M.on_hook(plugin_adapter, lsp_adapter, keymaps_adapter)
 
   return {}
 end
-
--- A dummy setup method. See on_hook.
-function M.setup(_) end
 
 return M
