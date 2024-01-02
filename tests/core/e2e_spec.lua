6d6a6b9c2fcb37acb472ecac109c37b33b7da395
abeldekat <abel@nomail.com>
2024-01-02 21:48:26 +0100

    e2e_spec. values adapter

--- tests/core/e2e_spec.lua
+++ tests/core/e2e_spec.lua
@@ -1,3 +1,8 @@
+--[[
+Test the adapters injecting code into LazyVim and lazy.nvim.
+--]]
+-- NOTE: --> lsp adapter: Not tested in e2e.
+
 local assert = require("luassert")
 local h = require("tests.unit_helpers")
 
@@ -17,6 +22,9 @@ describe("lazymenu.nvim", function()
     end
   end)
   it("integrates with LazyVim", function()
+    local function no_event()
+      return {}
+    end
     local leaders_to_change = { g = "G", s = "S" }
     local Lazy = require("lazy")
 
@@ -31,25 +39,43 @@ describe("lazymenu.nvim", function()
         "abeldekat/lazymenu.nvim",
         dir = get_dir(),
         import = "lazymenu.hook",
-        opts = function()
-          -- For some reason, the autocommand in on_load is not triggered inside the test
+        opts = function() -- WORKAROUNDS...
+          -- keymaps adapter: The autocommand in on_load is not triggered inside the test
           ---@diagnostic disable-next-line: duplicate-set-field
           require("lazymenu.adapters.utils").on_load = function(name, callback)
             callback(name) -- load immediately
           end
+
           return { leaders_to_change = leaders_to_change }
         end,
       },
       {
         "LazyVim/LazyVim",
         import = "lazyvim.plugins",
-        opts = function()
+        opts = function(_, _) -- WORKAROUNDS...
+          -- keymaps adapter: Trigger keymaps
           require("lazyvim.config.keymaps") -- on very lazy, UIEnter is too late
+
+          -- values adapter: trigger gitsigns. The test does not invoke gitsigns.opts
+          local gitsigns = require("lazy.core.config").spec.plugins["gitsigns.nvim"]
+          local gitsigns_opts = require("lazy.core.plugin").values(gitsigns, "opts", false)
+          gitsigns_opts.on_attach() -- manually invoke on_attach
         end,
+      },
+      {
+        "lewis6991/gitsigns.nvim", -- WORKAROUNDS...
+        event = no_event,
+        opts = {
+          -- values adapter: In LazyVim, gitsigns on_attach mappings are buffer local...
+          on_attach = function(_) -- override on_attach
+            vim.keymap.set({ "n" }, "<leader>ghS", function() end, { desc = "Test Stage Buffer" })
+          end,
+        },
       },
     }, { install_missing = true })
 
     assert(h.has_key(" So")) -- search options --> plugins adapter
     assert(h.has_key(" Gg")) -- keymaps.lua --> keymaps adapter
+    assert(h.has_key(" GhS")) -- gitsigns --> values adapter
   end)
 end)
